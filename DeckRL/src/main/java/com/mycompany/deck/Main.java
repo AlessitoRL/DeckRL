b3be829c0c535a3c9258fec9cddbb1b20fa6b535
Erick <ericklopez200384@gmail.com>
2023-10-02 20:50:37 -0600

    CARTITAS BONITAS

---
+++ DeckRL/src/main/java/com/mycompany/deck/Main.java
@@ -0,0 +1,15 @@
+
+package com.mycompany.deck;
+
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		Deck mazo = new Deck();
+
+		System.out.println("Mazo construido. Cantidad de cartas: " + mazo.getSize());
+
+	}
+
+}
